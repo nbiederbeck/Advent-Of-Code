2ea8e98a8c75c7124b5ca4804633304114d2e028
Noah Biederbeck <noah.biederbeck@udo.edu>
2018-12-03 08:33:28 +0100

    format code using yapf

--- 2018/dec01/solution.py
+++ 2018/dec01/solution.py
@@ -5,6 +5,7 @@ def a(path: str):
             offset += int(line)
     return offset
 
+
 def b(path: str):
     offset = 0
     frequencies = {offset}
@@ -16,37 +17,46 @@ def b(path: str):
                     return offset
                 frequencies.add(offset)
 
+
 def test_a1():
     f = "./dec01/test_a1"
     assert a(f) == 3
+
 
 def test_a2():
     f = "./dec01/test_a2"
     assert a(f) == 0
 
+
 def test_a3():
     f = "./dec01/test_a3"
     assert a(f) == -6
+
 
 def test_b1():
     f = "./dec01/test_b1"
     assert b(f) == 2
 
+
 def test_b2():
     f = "./dec01/test_b2"
     assert b(f) == 0
+
 
 def test_b3():
     f = "./dec01/test_b3"
     assert b(f) == 10
 
+
 def test_b4():
     f = "./dec01/test_b4"
     assert b(f) == 5
 
+
 def test_b5():
     f = "./dec01/test_b5"
     assert b(f) == 14
+
 
 if __name__ == "__main__":
     print(f"Day  1 (a): {a('./dec01/input_a')}")
